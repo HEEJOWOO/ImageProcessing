b5d752ec85c00c356449ef0511e03ece28fd8508
HEEJO WOO <61686244+HEEJOWOO@users.noreply.github.com>
2021-08-09 23:19:48 +0900

    Add files via upload

---
+++ 04_contours_edge_tracking_HSV.py
@@ -0,0 +1,42 @@
+import numpy as np
+import cv2
+##HSV field
+lowerBound = np.array([0, 180, 55])
+upperBound = np.array([20, 255, 200])
+
+cap = cv2.VideoCapture(0)
+
+cap.set(3, 640)
+cap.set(4, 480)
+
+while True:
+    ret, frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    color_mask = cv2.inRange(hsv, lowerBound, upperBound)
+
+    ret1, threshol = cv2.threshold(color_mask , 127, 255, 0)
+
+    contours, _ = cv2.findContours(threshol, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    if len(contours) > 0:
+        for i in range(len(contours)):
+            area = cv2.contourArea(contours[i])
+
+            if area > 100:
+                rect = cv2.minAreaRect(contours[i])
+                box = cv2.boxPoints(rect)
+                box = np.int0(box)
+                cv2.drawContours(frame, [box], 0, (0, 0, 255), 2)
+    if not ret:
+        print('error')
+        break
+    cv2.imshow('color_bitwise',color_mask)
+    cv2.imshow('cam_load',frame)
+    k = cv2.waitKey(1) & 0xFF
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
